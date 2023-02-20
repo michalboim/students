56f02f7e55ab6fa9b7020e8ebe8fc7a576a7343d
michalboim <123730033+michalboim@users.noreply.github.com>
2023-02-20 22:00:21 +0200

    start show course

--- app.py
+++ app.py
@@ -21,6 +21,18 @@ def administrator():
 def admin_courses():
     return render_template('admin_courses.html', courses_teachers=courses_teachers())
 
+@app.route('/course_info/<course_id>')
+def course_info(course_id):
+    course=crud.read_if('*',"courses","id", course_id)
+    course_object=create_courses_objects(course)
+    teacher_info=[]
+    for course in course_object:
+        for teacher in create_teachers_objects(crud.read_all('teachers')):
+            if course.teacher_id==str(teacher.tid):
+                teacher_info.append(teacher.tid)
+                teacher_info.append(teacher.name)
+    return render_template ('course_info.html', course_object=course_object, teacher_info=teacher_info )
+
 @app.route('/add_course', methods=['GET','POST'])
 def add_course():
     if request.method=='POST':  
@@ -28,11 +40,11 @@ def add_course():
         crud.create('courses', 'name, description, teacher_id, start, day, time', f" '{request.form['new_name'].title()}', '{request.form['new_description']}', '{request.form['teacher_tid']}', '{request.form['new_start']}', '{request.form['new_day']}', '{request.form['new_time']}' ")
         new_num=len(crud.read_all('courses'))
         if num_courses<new_num:
-            return render_template ('add_course.html', teachers_object=create_teachers_objects() ,note=f"{request.form['new_name'].title()} course added successfully")
+            return render_template ('add_course.html', teachers_object=create_teachers_objects(crud.read_all('teachers')) ,note=f"{request.form['new_name'].title()} course added successfully")
         else:
-            return render_template ('add_course.html',teachers_object=create_teachers_objects() ,note="A mistake occurred please try again")
+            return render_template ('add_course.html',teachers_object=create_teachers_objects(crud.read_all('teachers')) ,note="A mistake occurred please try again")
     else:
-        return render_template('add_course.html', teachers_object=create_teachers_objects())
+        return render_template('add_course.html', teachers_object=create_teachers_objects(crud.read_all('teachers')))
 
 @app.route('/update_courses', methods=['GET', 'POST'])
 def update_courses():
@@ -41,7 +53,7 @@ def update_courses():
         if len(courses_list)<1:
             return render_template('update_courses.html', result='No such course was found')
         if len(courses_list)>=1:
-            course_object=[classes.Course(course[0], course[1], course[2], course[3], course[4], course[5], course[6]) for course in courses_list]  
+            course_object=create_courses_objects(courses_list)  
             return render_template('update_courses.html',courses_objects=course_object)
     else:    
         return render_template('update_courses.html', courses=courses_teachers())
@@ -49,10 +61,10 @@ def update_courses():
 @app.route('/chosen_course/<course_id>', methods=['GET', 'POST'])
 def chosen_course_update(course_id):
     course_info=crud.read_if('*',"courses","id", course_id)
-    course_object=[classes.Course(course[0], course[1], course[2], course[3], course[4], course[5], course[6]) for course in course_info]
+    course_object=create_courses_objects(course_info)
     teacher_info=[]
     for course in course_object:
-        for teacher in create_teachers_objects():
+        for teacher in create_teachers_objects(crud.read_all('teachers')):
             if course.teacher_id==str(teacher.tid):
                 teacher_info.append(teacher.tid)
                 teacher_info.append(teacher.name)
@@ -66,11 +78,11 @@ def chosen_course_update(course_id):
         crud.update('courses', 'name, description, teacher_id, start, day, time', f"'{name}', '{description}', '{teacher_id}', '{start}', '{day}', '{time}'", course_id)
         return redirect(url_for('admin_courses'))
     else:
-        return render_template('chosen_course.html',course_object=course_object, teachers_objects=create_teachers_objects(), teacher_info=teacher_info ) 
+        return render_template('chosen_course.html',course_object=course_object, teachers_objects=create_teachers_objects(crud.read_all('teachers')), teacher_info=teacher_info ) 
 
 @app.route('/admin_students')
 def admin_students():
-    return render_template('admin_students.html', students_objects=create_students_objects())
+    return render_template('admin_students.html', students_objects=create_students_objects(crud.read_all('students')))
 
 @app.route('/add_student', methods=['POST','GET'])
 def add_student():
@@ -92,15 +104,15 @@ def update_students():
         if len(students_list)<1:
             return render_template('update_students.html', result='No such student was found')
         if len(students_list)>=1:
-            student_object=[classes.Student(student[0], student[1], student[2], student[3]) for student in students_list]  
+            student_object=create_students_objects(students_list) 
             return render_template('update_students.html',student_objects=student_object)
     else:    
-        return render_template('update_students.html', students=create_students_objects())
+        return render_template('update_students.html', students=create_students_objects(crud.read_all('students')))
 
 @app.route('/chosen_student/<student_id>', methods=['GET', 'POST'])
 def chosen_student_update(student_id):
     student_info=crud.read_if('*',"students","id", student_id)
-    student_object=[classes.Student(student[0], student[1], student[2], student[3]) for student in student_info]
+    student_object=create_students_objects(student_info)
     if request.method=='POST':
         name=request.form['name'].title()
         email=request.form['email']
@@ -110,10 +122,9 @@ def chosen_student_update(student_id):
     else:
         return render_template('chosen_student.html',student_object=student_object) 
 
-
 @app.route('/admin_teachers')
 def admin_teachers():
-    return render_template('admin_teachers.html', teachers_objects=create_teachers_objects())
+    return render_template('admin_teachers.html', teachers_objects=create_teachers_objects(crud.read_all('teachers')))
 
 @app.route('/add_teacher', methods=['POST','GET'])
 def add_teacher():
@@ -135,15 +146,15 @@ def update_teachers():
         if len(teachers_list)<1:
             return render_template('update_teachers.html', result='No such teacher was found')
         if len(teachers_list)>=1:
-            teacher_object=[classes.Teacher(teacher[0], teacher[1], teacher[2], teacher[3]) for teacher in teachers_list]  
+            teacher_object=create_teachers_objects(teachers_list)   
             return render_template('update_teachers.html',teacher_objects=teacher_object)
     else:    
-        return render_template('update_teachers.html', teachers=create_teachers_objects())
+        return render_template('update_teachers.html', teachers=create_teachers_objects(crud.read_all('teachers')))
 
 @app.route('/chosen_teacher/<teacher_id>', methods=['GET', 'POST'])
 def chosen_teacher_update(teacher_id):
     teacher_info=crud.read_if('*',"teachers","id", teacher_id)
-    teacher_object=[classes.Teacher(teacher[0], teacher[1], teacher[2], teacher[3]) for teacher in teacher_info]
+    teacher_object=create_teachers_objects(teacher_info) 
     if request.method=='POST':
         name=request.form['name'].title()
         email=request.form['email']
--- functions.py
+++ functions.py
@@ -1,25 +1,22 @@
 import classes
 import crud
 
-def create_courses_objects():
-    courses_list=crud.read_all('courses')
-    courses_objects=[classes.Course(course[0], course[1], course[2], course[3], course[4], course[5], course[6] ) for course in courses_list]
+def create_courses_objects(courses: list):
+    courses_objects=[classes.Course(course[0], course[1], course[2], course[3], course[4], course[5], course[6] ) for course in courses]
     return courses_objects
 
-def create_teachers_objects():
-    teachers_list=crud.read_all('teachers')
-    teachers_objects=[classes.Teacher(teacher[0], teacher[1], teacher[2], teacher[3]) for teacher in teachers_list]
+def create_teachers_objects(teachers: list):
+    teachers_objects=[classes.Teacher(teacher[0], teacher[1], teacher[2], teacher[3]) for teacher in teachers]
     return teachers_objects
 
-def create_students_objects():
-    students_list=crud.read_all('students')
-    students_objects=[classes.Student(student[0], student[1], student[2], student[3]) for student in students_list]
+def create_students_objects(students: list):
+    students_objects=[classes.Student(student[0], student[1], student[2], student[3]) for student in students]
     return students_objects
 
 def courses_teachers():
     courses_teachers=[]
-    for course in create_courses_objects():
-        for teacher in create_teachers_objects():
+    for course in create_courses_objects(crud.read_all('courses')):
+        for teacher in create_teachers_objects(crud.read_all('teachers')):
             if course.teacher_id==str(teacher.tid):
                 course_teacher=classes.Course(course.tid, course.name, course.description, teacher.name, course.start, course.day, course.time)
                 courses_teachers.append(course_teacher)
